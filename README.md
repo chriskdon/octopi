4868d0c98449b6ae613c2e0367008ae6b0644b73
Chris Kellendonk <chriskellendonk@gmail.com>
2015-08-27 00:58:56 -0400

    Initial project setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# Octopi
+
+A jpath parsing library similar to xpath to allow quick access and retrieval of data
+from complex javascript objects with a simple and expressive syntax.
---
+++ tests/general.js
@@ -0,0 +1,24 @@
+var octopi = require('../index');
+var should = require('should');
+
+var testObj = {
+  flat: true,
+  nested: {
+    base: true,
+    layer: {
+      value: 3
+    }
+  }
+};
+
+describe('octopi', function () {
+  it('should access value with dot notation', function () {
+    var result1 = octopi.parse("$.flat", testObj);
+    var result2 = octopi.parse("$.nested.base", testObj);
+    var result3 = octopi.parse("$.nested.layer.value", testObj);
+
+    result1.should.equal(testObj.flat);
+    result2.should.equal(testObj.nested.base);
+    result3.should.equal(testObj.nested.layer.value);
+  });
+});
